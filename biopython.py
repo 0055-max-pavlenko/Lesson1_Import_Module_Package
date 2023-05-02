ccf36fe140ce19c93fb0bcbf9d318b21672e4cdd
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-05-02 22:00:34 +0300

    02/05/2023

---
+++ biopython.py
@@ -0,0 +1,13 @@
+﻿from Bio.Seq import Seq
+
+
+
+def print_protein_sequence_from_DNA (DNA):
+    coding_DNA = Seq(DNA)
+    protein_seq = coding_DNA.translate()
+    print(f'Последовательность кодирующей ДНК: {coding_DNA}\n')
+    print(f'Последовательность кодируемого белка: {protein_seq}')
+
+DNA = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+
+print_protein_sequence_from_DNA(DNA)
---
+++ main.py
@@ -0,0 +1,8 @@
+﻿from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+
+
+if __name__ == '__main__':
+    print(f"Сегодня: {datetime.now().strftime('%d %B %Y')}")
